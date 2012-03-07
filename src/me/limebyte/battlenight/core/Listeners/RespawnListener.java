83dcd9750a40b2264811182a12bbc8f8fcd8b3fd
LimeByte <limebyte@twf.co.nz>
2012-03-07 17:30:55 +1300

    Tidy up.

--- src/me/limebyte/battlenight/core/Listeners/RespawnListener.java
+++ src/me/limebyte/battlenight/core/Listeners/RespawnListener.java
@@ -2,7 +2,6 @@ package me.limebyte.battlenight.core.Listeners;
 
 import me.limebyte.battlenight.core.BattleNight;
 
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -21,16 +20,14 @@ public class RespawnListener implements Listener {
 	public void onPlayerRespawn(PlayerRespawnEvent event) {
 		Player player = event.getPlayer();
 		if (plugin.BattleUsersRespawn.containsKey(player.getName())) {
-			Location spectator = plugin.getCoords("spectator");
-			Location exit = plugin.getCoords("exit");
 			
 			// If the Battle is still going on, take them to the spectator area to watch
 			if(plugin.battleInProgress){
-				event.setRespawnLocation(spectator);
+				event.setRespawnLocation(plugin.getCoords("spectator"));
 				plugin.addSpectator(player, "death");
 			// Else, take them to the exit area
 			} else {
-				event.setRespawnLocation(exit);
+				event.setRespawnLocation(plugin.getCoords("exit"));
 			}
 
 			plugin.BattleUsersRespawn.remove(player.getName());
