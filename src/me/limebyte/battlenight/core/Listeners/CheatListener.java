09962ae3827cc1f66b79d0146234f6a64237c8ef
LimeByte <limebyte@twf.co.nz>
2012-03-27 16:27:22 +1300

    Added Teleportation options.

--- src/me/limebyte/battlenight/core/Listeners/CheatListener.java
+++ src/me/limebyte/battlenight/core/Listeners/CheatListener.java
@@ -3,7 +3,6 @@ package me.limebyte.battlenight.core.Listeners;
 import me.limebyte.battlenight.core.BattleNight;
 import me.limebyte.battlenight.core.Other.Tracks.Track;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
@@ -27,11 +26,28 @@ public class CheatListener implements Listener {
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerTeleport(PlayerTeleportEvent event) {
 		Player player = event.getPlayer();
-
-		if ((plugin.BattleUsersTeam.containsKey(player.getName())) && 
-				(!plugin.BattleTelePass.containsKey(player.getName()))) {
-			event.setCancelled(true);
-			plugin.tellPlayer(player, Track.NO_TP);
+		if (plugin.BattleUsersTeam.containsKey(player.getName()) && !plugin.BattleTelePass.containsKey(player.getName())) {
+			switch (event.getCause()) {
+				case COMMAND: case PLUGIN:
+					event.setCancelled(true);
+					plugin.tellPlayer(player, Track.NO_TP);
+					break;
+				case ENDER_PEARL:;
+					if (!plugin.config.getBoolean("Teleportation.EnderPearls", true)) {
+						event.setCancelled(true);
+						plugin.tellPlayer(player, Track.NO_TP);
+					}
+					break;
+				case NETHER_PORTAL: case END_PORTAL:
+					if (!plugin.config.getBoolean("Teleportation.Portals", false)) {
+						event.setCancelled(true);
+						plugin.tellPlayer(player, Track.NO_TP);
+					}
+					break;
+				default:
+					event.setCancelled(true);
+					break;
+			}
 		}
 	}
 	
@@ -46,7 +62,7 @@ public class CheatListener implements Listener {
 				Player thrower = (Player) projectile.getShooter();
 				if (plugin.BattleUsersTeam.containsKey(thrower.getName()) && plugin.playersInLounge) {
 					event.setCancelled(true);
-					thrower.sendMessage(ChatColor.GRAY + "[BattleNight] " + ChatColor.WHITE + "Not so fast! No Cheating!");
+					plugin.tellPlayer(thrower, Track.NO_CHEATING);
 				}
 			}
 	}
