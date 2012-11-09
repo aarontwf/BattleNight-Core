79128af09c3afc074d9bc0f90c4e3fadc5ef3f59
LimeByte <limebyte@twf.co.nz>
2012-11-10 12:00:01 +1300

    Equip items a different way.

--- src/main/java/me/limebyte/battlenight/core/util/BattleClass.java
+++ src/main/java/me/limebyte/battlenight/core/util/BattleClass.java
@@ -10,6 +10,8 @@ public class BattleClass {
     private String name;
     private List<ItemStack> items, armour;
 
+    private static final int LAST_INV_SLOT = 35;
+
     public BattleClass(String name, List<ItemStack> items, List<ItemStack> armour) {
         this.name = name;
         this.items = items;
@@ -31,7 +33,13 @@ public class BattleClass {
     public void equip(Player player) {
         PlayerInventory inv = player.getInventory();
 
-        inv.setContents(items.toArray(new ItemStack[items.size()]));
+        // Main Inventory
+        for (int i = 0; i < items.size(); i++) {
+            if (i > LAST_INV_SLOT) break;
+            inv.setItem(i, items.get(i));
+        }
+
+        // Armour
         inv.setHelmet(armour.get(0));
         inv.setChestplate(armour.get(1));
         inv.setLeggings(armour.get(2));
