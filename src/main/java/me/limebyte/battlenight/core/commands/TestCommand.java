d6353af8b92bc36abcd55c7e6ab74aec2bde8994
LimeByte <limebyte@twf.co.nz>
2013-01-06 16:34:18 +1300

    Added test commands to test the Arena system.

--- src/main/java/me/limebyte/battlenight/core/commands/TestCommand.java
+++ src/main/java/me/limebyte/battlenight/core/commands/TestCommand.java
@@ -2,6 +2,9 @@ package me.limebyte.battlenight.core.commands;
 
 import java.util.Arrays;
 
+import me.limebyte.battlenight.api.BattleNightAPI;
+import me.limebyte.battlenight.api.battle.Arena;
+import me.limebyte.battlenight.api.battle.Waypoint;
 import me.limebyte.battlenight.api.util.BattleNightCommand;
 import me.limebyte.battlenight.core.BattleNight;
 import me.limebyte.battlenight.core.util.Messenger;
@@ -24,6 +27,7 @@ public class TestCommand extends BattleNightCommand {
 
     @Override
     protected boolean onPerformed(CommandSender sender, String[] args) {
+        // Commands with 1 argument
         if (args.length < 1) {
             Messenger.tell(sender, Message.SPECIFY_TEST);
             Messenger.tell(sender, Message.USAGE, getUsage());
@@ -31,22 +35,61 @@ public class TestCommand extends BattleNightCommand {
         }
 
         if (args[0].equalsIgnoreCase("join")) {
-            if (sender instanceof Player) {
-                BattleNight.instance.getAPI().getBattle().addPlayer((Player) sender);
-                return true;
-            } else {
-                Messenger.tell(sender, Message.PLAYER_ONLY);
-                return false;
-            }
+            if (isNotPlayer(sender)) return false;
+            BattleNight.instance.getAPI().getBattle().addPlayer((Player) sender);
+            return true;
         } else if (args[0].equalsIgnoreCase("leave")) {
-            if (sender instanceof Player) {
-                BattleNight.instance.getAPI().getBattle().removePlayer((Player) sender);
-                return true;
-            } else {
-                Messenger.tell(sender, Message.PLAYER_ONLY);
+            if (isNotPlayer(sender)) return false;
+            BattleNight.instance.getAPI().getBattle().removePlayer((Player) sender);
+            return true;
+        }
+
+        // Commands with 2 arguments
+        if (args.length < 2) {
+            Messenger.tell(sender, Message.INCORRECT_USAGE);
+            return false;
+        }
+
+        if (args[0].equalsIgnoreCase("create")) {
+            BattleNightAPI api = BattleNight.instance.getAPI();
+            for (Arena arena : api.getArenas()) {
+                if (arena.getName().equals(args[1])) {
+                    Messenger.tell(sender, "An Arena by that name already exists!");
+                    return false;
+                }
+            }
+            api.registerArena(new Arena(args[1]));
+            Messenger.tell(sender, "Arena " + args[1] + " created.");
+            return true;
+        } else if (args[0].equalsIgnoreCase("addspawn")) {
+            if (isNotPlayer(sender)) return false;
+            BattleNightAPI api = BattleNight.instance.getAPI();
+            Arena arena = null;
+
+            for (Arena a : api.getArenas()) {
+                if (a.getName().equals(args[1])) {
+                    arena = a;
+                }
+            }
+
+            if (arena == null) {
+                Messenger.tell(sender, "An Arena by that name does not exist!");
                 return false;
             }
-        } else return false;
+            Waypoint point = new Waypoint("spawnpoint");
+            arena.addSpawnPoint(point);
+            Messenger.tell(sender, "Spawn point created.");
+            return true;
+        }
+
+        return false;
     }
 
+    private boolean isNotPlayer(CommandSender sender) {
+        if (!(sender instanceof Player)) {
+            Messenger.tell(sender, Message.PLAYER_ONLY);
+            return true;
+        }
+        return false;
+    }
 }
