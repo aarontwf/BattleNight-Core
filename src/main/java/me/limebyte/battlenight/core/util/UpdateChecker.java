26191366f03449b4286b7aeeeb472e71ed2727f5
LimeByte <limebyte@twf.co.nz>
2013-11-21 22:15:44 +1300

    Use BukkitDev for update checking.

--- src/main/java/me/limebyte/battlenight/core/util/UpdateChecker.java
+++ src/main/java/me/limebyte/battlenight/core/util/UpdateChecker.java
@@ -3,17 +3,27 @@ package me.limebyte.battlenight.core.util;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.logging.Level;
 
 import me.limebyte.battlenight.api.BattleNightAPI;
 import me.limebyte.battlenight.api.util.Messenger;
 
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 public class UpdateChecker {
 
-    private static final String VERSION_URL = "https://raw.github.com/BattleNight/BattleNight-Core/master/version.txt";
-    private static final String UPDATE_MESSAGE = "A new version of BattleNight has been released.  You can download v%s from %s.";
+    private static final int PROJECT_ID = 31742;
+
+    private static final String API_HOST = "https://api.curseforge.com/servermods/files?projectIds=";
+    private static final String API_NAME_VALUE = "name";
+    private static final String API_LINK_VALUE = "downloadUrl";
+    private static final String API_GAME_VERSION_VALUE = "gameVersion";
+
+    private static final String UPDATE_MESSAGE = "A new version of BattleNight has been released.  You can download v%s for %s from %s.";
 
     private static String version;
     private static boolean snapshot;
@@ -28,19 +38,29 @@ public class UpdateChecker {
         Messenger messenger = api.getMessenger();
 
         try {
-            URL update = new URL(VERSION_URL);
-            BufferedReader in = new BufferedReader(new InputStreamReader(update.openStream()));
-            String latestVersion = in.readLine().trim();
-            String downloadURL = in.readLine().trim();
-            in.close();
+            URL url = new URL(API_HOST + PROJECT_ID);
+            URLConnection conn = url.openConnection();
 
-            if (isNewer(latestVersion)) {
+            conn.addRequestProperty("User-Agent", "BattleNight Update Checker");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String response = reader.readLine();
+
+            JSONArray array = (JSONArray) JSONValue.parse(response);
+            JSONObject latest = (JSONObject) array.get(array.size() - 1);
+
+            String version = ((String) latest.get(API_NAME_VALUE)).replace("BattleNight v", "");
+            String downloadUrl = (String) latest.get(API_LINK_VALUE);
+            String gameVersion = (String) latest.get(API_GAME_VERSION_VALUE);
+
+            if (isNewer(version)) {
                 updateAvailable = true;
-                updateMessage = String.format(UPDATE_MESSAGE, latestVersion, downloadURL);
+                updateMessage = String.format(UPDATE_MESSAGE, version, gameVersion, downloadUrl);
                 messenger.log(Level.INFO, updateMessage);
             }
         } catch (Exception e) {
             messenger.debug(Level.WARNING, "Failed to check for updates.");
+            messenger.debug(Level.WARNING, e.getMessage());
             return;
         }
     }
