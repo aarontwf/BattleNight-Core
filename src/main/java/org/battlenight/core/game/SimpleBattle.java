1e8a3b60b1d054bb7ab1ef6ca879fd48652b846b
LimeByte <limebyte@twf.co.nz>
2014-01-25 13:30:56 +1300

    Update Maven plugin versions

--- src/main/java/org/battlenight/core/game/SimpleBattle.java
+++ src/main/java/org/battlenight/core/game/SimpleBattle.java
@@ -4,16 +4,16 @@ import org.bukkit.entity.Player;
 
 public class SimpleBattle {
 
-	public SimpleBattle() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public void addPlayer(Player player) {
-		
-	}
-	
-	public void removePlayer(Player player) {
-		
-	}
-	
+    public SimpleBattle() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void addPlayer(Player player) {
+
+    }
+
+    public void removePlayer(Player player) {
+
+    }
+
 }
